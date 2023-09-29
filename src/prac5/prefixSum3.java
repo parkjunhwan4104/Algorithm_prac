9e8db0491bc5293425ab519ce607c783366ab865
unknown <wnsghks4104@gmail.com>
2023-09-29 20:13:16 +0900

    prefixSum practice3

--- src/prac5/prefixSum3.java
+++ src/prac5/prefixSum3.java
@@ -9,10 +9,10 @@ import java.util.StringTokenizer;
 
 public class prefixSum3 {
 	
-	static int[][] range;
-	static String[] alpha;
+	static int[][] alphabetSum;
 	static char[] sentence;
 	static int[] result;
+	
 	
 	public static void main(String[] args) throws IOException{
 		
@@ -26,21 +26,45 @@ public class prefixSum3 {
 		StringTokenizer st;
 		
 		int n=Integer.parseInt(br.readLine());
+		int l=sentence.length;
 		
-		range=new int[n][2];
-		alpha=new String[n];
-		result=new int[sentence.length];
-				
+		alphabetSum=new int[l+1][26];
+		result=new int[n];
+	
 		
-		for(int i=0;i<n;i++) {
-			st=new StringTokenizer(br.readLine()," ");
-			alpha[i]=st.nextToken();
+		for(int i=1;i<=l;i++) {
 			
-			range[i][0]=Integer.parseInt(st.nextToken());
-			range[i][1]=Integer.parseInt(st.nextToken());
-							
+			int index=sentence[i-1]-'a';   //어떤 알파벳인지에 대한 인덱스번호
+			
+			for(int j=0;j<26;j++) {
+				
+				alphabetSum[i][j]=alphabetSum[i-1][j];
+				
+				if(index==j) {
+					alphabetSum[i][j]=alphabetSum[i-1][j]+1;
+				}
+			}
+			
 		}
 	
+		for(int i=0;i<n;i++) {
+			st=new StringTokenizer(br.readLine()," ");
+			int index=st.nextToken().charAt(0)-'a';
+			
+			int start=Integer.parseInt(st.nextToken())+1;
+			int end=Integer.parseInt(st.nextToken())+1;
+					
+			result[i]=alphabetSum[end][index]-alphabetSum[start-1][index];
+			
+			
+		}
+		
+		for(int i=0;i<n;i++) {
+			bw.write(result[i]+"\n");
+		}
+	
+		bw.flush();
+		bw.close();
 	}
 	
 	
